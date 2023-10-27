bf099cebb3a2726879e4953032b92e64646886b1
Danial <danialhatami94@gmail.com>
2023-10-27 15:05:23 +0330

    add transaction observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Transaction;
 use Dedoc\Scramble\Scramble;
+use App\Observers\TransactionObserver;
 use Illuminate\Support\ServiceProvider;
 use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
@@ -22,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Observers
+        Transaction::observe(TransactionObserver::class);
+
+        // Api document
         Scramble::extendOpenApi(function (OpenApi $openApi) {
             $openApi->secure(
                 SecurityScheme::oauth2()
