a18d603d95e00e43856a9e9fec7a692f41625dbb
Danial <danialhatami94@gmail.com>
2023-10-27 15:07:20 +0330

    modify to create 10 products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use App\Models\Product;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -20,6 +18,6 @@ class ProductSeeder extends Seeder
                 'name' => 'Product Number one'
             ]
         )
-            ->create();
+            ->count(10)->create();
     }
 }
