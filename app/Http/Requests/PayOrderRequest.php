c160f94bd13bbceff3a202e52c5e63d48be07430
Danial <danialhatami94@gmail.com>
2023-10-27 15:19:33 +0330

    authorize logic for PayOrderReqeust

---
+++ app/Http/Requests/PayOrderRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PayOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $order = $this->route('order');
+        return $this->user()->id === $order->user_id;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
