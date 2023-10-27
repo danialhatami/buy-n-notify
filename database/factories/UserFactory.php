607b835d21be5af82de4553d76dea8ac8a70430a
Danial <danialhatami94@gmail.com>
2023-10-27 15:08:24 +0330

    fix email duplication in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => 'test@mail.com',
+            'email' => fake()->email(),
             'email_verified_at' => now(),
             'mobile' => fake()->numerify('###########'),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
