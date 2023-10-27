acab618f116e62b04ca539e6f6562843ebec8a62
Danial <danialhatami94@gmail.com>
2023-10-27 15:15:59 +0330

    add notification for send message after order paid

---
+++ app/Notifications/OrderPaid.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use App\Traits\HighPriorityMessage;
+use App\Notifications\Channels\SmsChannel;
+
+class OrderPaid extends CustomNotification
+{
+    use HighPriorityMessage;
+    use Queueable;
+
+    public int $tries = 1;
+    public int $maxExceptions = 1;
+
+    public function __construct(private readonly string $orderId)
+    {
+    }
+
+    public function text(): string
+    {
+        return trans('order.sms.order_paid', ['code' => $this->orderId]);
+    }
+
+    public function via(): string
+    {
+        return SmsChannel::class;
+    }
+
+    public function payload(object $notifiable): array
+    {
+        return [
+            'to' => $notifiable->mobile,
+            'message' => $this->text()
+        ];
+    }
+}
