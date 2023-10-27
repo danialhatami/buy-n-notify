a3c19d1ae7c671ef9e8e4e9981b52073a8b3d74c
Danial <danialhatami94@gmail.com>
2023-10-27 14:03:57 +0330

    modify TestCast: modify setup and add seeder

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,16 @@
 
 namespace Tests;
 
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(DatabaseSeeder::class);
+    }
 }
